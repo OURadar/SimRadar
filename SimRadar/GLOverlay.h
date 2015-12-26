5373d6638daa9222613ed9a27ed087a44a73b74f
boonleng <boonleng@ou.edu>
2015-12-26 15:44:34 -0600

    Getting ready to add a status overlay

---
+++ SimRadar/GLOverlay.h
@@ -0,0 +1,16 @@
+//
+//  GLOverlay.h
+//
+//  Created by Boon Leng Cheong on 12/26/15.
+//  Copyright © 2015 Boon Leng Cheong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OpenGL/OpenGL.h>
+#import <OpenGL/gl3.h>
+#import <GLKit/GLKit.h>
+#import <Quartz/Quartz.h>
+
+@interface GLOverlay : NSObject
+
+@end
---
+++ SimRadar/GLOverlay.m
@@ -0,0 +1,12 @@
+//
+//  GLOverlay.m
+//
+//  Created by Boon Leng Cheong on 12/26/15.
+//  Copyright © 2015 Boon Leng Cheong. All rights reserved.
+//
+
+#import "GLOverlay.h"
+
+@implementation GLOverlay
+
+@end
