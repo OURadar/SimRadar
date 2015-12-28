c03b7a2210539b7f19110a659723fc6627c7d07b
boonleng <boonleng@ou.edu>
2015-12-28 11:24:51 -0600

    Added theory of operations comment. Still thinking about the architecture...

--- SimRadar/GLOverlay.m
+++ SimRadar/GLOverlay.m
@@ -1,6 +1,13 @@
 //
 //  GLOverlay.m
 //
+//  Theory of Operations
+//
+//  - Instantiate the class object at anytime since it is Core Graphic based
+//  - Draw / update to the CG context after initialization (separate thread)
+//  - During OpenGL rendering, call the method updateGLTexture
+//  - Then, call the method drawAtRect:, which draws the rectangular overlay
+//
 //  Created by Boon Leng Cheong on 12/26/15.
 //  Copyright © 2015 Boon Leng Cheong. All rights reserved.
 //
@@ -12,13 +19,14 @@
 - (id)initWithSize:(NSSize)size {
     self = [super init];
     if (self) {
-        // Initialize a Core-graphic context of size
+        // Initialize a Core-graphic context of given size
     }
     return self;
 }
 
 - (int)updateGLTexture {
-    
+    // Include vertex & fragment shader GLSL here
+    return 0;
 }
 
 - (void)drawAtRect:(NSRect)rect {
