9b0364a848d688059178d3431d721e4417b07969
boonleng <boonleng@ou.edu>
2016-11-25 11:48:20 -0600

    All updated

--- rs.h
+++ rs.h
@@ -506,22 +506,22 @@ void RS_set_angular_weight(RSHandle *H, const float *weights, const float table_
 void RS_set_angular_weight_to_standard(RSHandle *H, float beamwidth_deg);
 void RS_set_angular_weight_to_double_cone(RSHandle *H, float beamwidth_deg);
 
-//void RS_set_vel_data_to_config(RSHandle *, LESConfig);
-//void RS_set_vel_data_to_LES_table(RSHandle *H, const LESTable *table);
-//void RS_set_vel_data_to_uniform(RSHandle *H, cl_float4 velocity);
-//void RS_set_vel_data_to_cube27(RSHandle *H);
-//void RS_set_vel_data_to_cube125(RSHandle *H);
-//void RS_clear_vel_data(RSHandle *H);
-//
-//void RS_set_adm_data_to_config(RSHandle *, ADMConfig);
-//void RS_set_adm_data_to_ADM_table(RSHandle *H, const ADMTable *table);
-//void RS_set_adm_data_to_unity(RSHandle *H);
-//void RS_clear_adm_data(RSHandle *H);
-//
-//void RS_set_rcs_data_to_config(RSHandle *, RCSConfig);
-//void RS_set_rcs_data_to_RCS_table(RSHandle *H, const RCSTable *table);
-//void RS_set_rcs_data_to_unity(RSHandle *H);
-//void RS_clear_rcs_data(RSHandle *H);
+void RS_set_vel_data_to_config(RSHandle *, LESConfig);
+void RS_set_vel_data_to_LES_table(RSHandle *H, const LESTable *table);
+void RS_set_vel_data_to_uniform(RSHandle *H, cl_float4 velocity);
+void RS_set_vel_data_to_cube27(RSHandle *H);
+void RS_set_vel_data_to_cube125(RSHandle *H);
+void RS_clear_vel_data(RSHandle *H);
+
+void RS_set_adm_data_to_config(RSHandle *, ADMConfig);
+void RS_set_adm_data_to_ADM_table(RSHandle *H, const ADMTable *table);
+void RS_set_adm_data_to_unity(RSHandle *H);
+void RS_clear_adm_data(RSHandle *H);
+
+void RS_set_rcs_data_to_config(RSHandle *, RCSConfig);
+void RS_set_rcs_data_to_RCS_table(RSHandle *H, const RCSTable *table);
+void RS_set_rcs_data_to_unity(RSHandle *H);
+void RS_clear_rcs_data(RSHandle *H);
 
 // New methods
 void RS_set_obj_data_to_config(RSHandle *, OBJConfig);
