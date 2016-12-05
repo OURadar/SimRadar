6ec794776707fa3400fbda0b5a552ed42f75d3cf
boonleng <boonleng@ou.edu>
2016-12-05 13:15:45 -0600

    More tweaks for schooner

--- r.sh
+++ r.sh
@@ -11,12 +11,14 @@
 #SBATCH --mail-type=ALL
 #SBATCH --workdir=/home/boonleng/simradar
 
+module load OpenCL/2.2-GCC-4.9.3-2.25
 module load CUDA/8.0.44-GCC-4.9.3-2.25
 module load OpenMPI/1.10.2-GCC-4.9.3-2.25
 
-export LD_LIBRARY=/opt/oscer/software/OpenCL/2.2-GCC-4.9.3-2.25/lib64
+export LD_LIBRARY=/opt/oscer/software/OpenCL/2.2-GCC-4.9.3-2.25/lib64:/opt/oscer/software/OpenMPI/1.10.2-GCC-4.9.3-2.25/lib
 
 cd /home/boonleng/simradar
 
 #run_big_mpi.sh
 radarsim -v -p 2400 -O ${HOME}/Downloads/big/ --tightbox --concept DB -W 1000
+
