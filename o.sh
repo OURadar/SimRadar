f49f3bcc6ec9c4fffda4db7c9bcf3459fb4c13ff
boonleng <boonleng@ou.edu>
2019-03-14 16:00:18 -0500

    Updated log inspection script

--- o.sh
+++ o.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
+function reset() {
+	echo -e "\033[0m"
+}
+
+function green() {
+	echo -e "\033[38;5;82m"
+}
+
+function red() {
+	echo -e "\033[38;5;203m"
+}
+
 function show_last_seg() {
 	cmd="grep -n \"==================<<<\" \"$1\" | tail -n 1 | awk -F \":\" '{print \$1}'"
-	line=`eval $cmd`
-        if [ ! -z ${line} ]; then
-		tail -n +$line $1
+	line=$(eval ${cmd})
+	if [ ! -z ${line} ]; then
+		tail -n +$((line)) $1 | head -n 2
+		reset
+		line=$((line+2))
+		tail -n +$((line)) $1
 	fi
 }
 
-
 if [[ "$#" -gt 0 && "$1" == "t" ]]; then
 	# Test
-	echo -e "\033[33m"
+	green
 	show_last_seg tests_stdout.txt
-	echo -e "\033[31m"
+	red
 	show_last_seg tests_stderr.txt
 elif [[ "$#" -gt 0 && "$1" == "1" ]]; then
 	# Single node execution
-	echo -e "\033[32m"
-	show_last_seg radarsim_single_stdout.txt
-	echo -e "\033[31m"
-	show_last_seg radarsim_single_stderr.txt
+	green
+	show_last_seg simradar_single_stdout.txt
+	red
+	show_last_seg simradar_single_stderr.txt
 elif [[ "$#" -gt 0 && "$1" == "c" ]]; then
 	echo -e "\033[36m"
-	show_last_seg radarsim_cpu.txt
+	show_last_seg simradar_cpu.txt
 else
-	echo -e "\033[32m"
-	show_last_seg radarsim_stdout.txt
-	echo -e "\033[31m"
-	show_last_seg radarsim_stderr.txt
+	green
+	show_last_seg simradar_stdout.txt
+	red
+	show_last_seg simradar_stderr.txt
 fi
 
 echo -e "\033[0m"
