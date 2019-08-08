4cb2cb4476579dae645135a7d2d3574136fe95eb
boonleng <boonleng@ou.edu>
2019-08-08 10:11:10 -0500

    Merged rsutil and test_rs

--- rsutil.c
+++ rsutil.c
@@ -4,12 +4,35 @@
 #include <dirent.h>
 #include <errno.h>
 
-#define ITALIC(x)            "\033[3m" x "\033[23m"
-#define UNDERLINE(x)         "\033[4m" x "\033[24m"
-#define HIGHLIGHT(x)         "\033[38;5;82;48;5;238m" x "\033[0m"
-#define UNDERLINE_ITALIC(x)  "\033[3;4m" x "\033[23;24m"
+#define CL_SILENCE_DEPRECATION    (1)
+
+#define ITALIC(x)                 "\033[3m" x "\033[23m"
+#define UNDERLINE(x)              "\033[4m" x "\033[24m"
+#define HIGHLIGHT(x)              "\033[38;5;82;48;5;238m" x "\033[0m"
+#define UNDERLINE_ITALIC(x)       "\033[3;4m" x "\033[23;24m"
+
+#define FMT                       "%30s  %8.4f ms\n"
+#define FMT2                      "%30s  %8.4f ms   %6.2f GB/s\n"
 
 #
+
+enum {
+    TEST_N0NE                   = 0,
+    TEST_ADVANCE_TIME_GPU       = 1,
+    TEST_MAKE_PULSE_GPU         = 1 << 1,
+    TEST_MAKE_PULSE_GPU_PASS_1  = 1 << 2,
+    TEST_MAKE_PULSE_GPU_PASS_2  = 1 << 3,
+    TEST_DOWNLOAD               = 1 << 4,
+    TEST_IO                     = 1 << 5,
+    TEST_DUMMY                  = 1 << 6,
+    TEST_SIG_AUX                = 1 << 7,
+    TEST_BG_ATTS                = 1 << 8,
+    TEST_EL_ATTS                = 1 << 9,
+    TEST_DB_ATTS                = 1 << 10,
+    TEST_KERNEL_MASK            = (TEST_BG_ATTS | TEST_EL_ATTS | TEST_DB_ATTS | TEST_SIG_AUX | TEST_MAKE_PULSE_GPU_PASS_1 | TEST_MAKE_PULSE_GPU_PASS_2),
+    TEST_SIMPLE                 = (TEST_ADVANCE_TIME_GPU | TEST_MAKE_PULSE_GPU | TEST_DOWNLOAD | TEST_IO),
+    TEST_ALL                    = (TEST_SIMPLE | TEST_KERNEL_MASK),
+};
 
 void set_debris_flux_field_to_test(RSHandle *H) {
     // CDF: 3 x 3 flux field with a middle cell at 1.0 (cell 4)
@@ -152,27 +175,324 @@ int RS_indent_copy(char *dst, char *src, const int width) {
     return k;
 }
 
-char *RS_test_by_number_description(const int indent) {
-    static char text[4096];
-    char helpText[] =
-    " 0 - Generate iCDF from CDF\n";
-    RS_indent_copy(text, helpText, indent);
-    if (strlen(text) > 3000) {
-        fprintf(stderr, "Warning. Approaching limit. (%lu)\n", strlen(text));
+static void RS_test_by_flags(const uint32_t flag, const uint32_t verb) {
+    float density = 0.0f;
+    unsigned int N = 0;
+    
+    struct timeval t1, t2;
+
+    if (flag & TEST_ALL && N == 0) {
+        N = 100;
     }
-    return text;
-}
+    
+    int i;
+    double dt;
+    size_t byte_size;
+    
+    RSHandle *H = RS_init_verbose(verb);
+    if (H == NULL) {
+        fprintf(stderr, "%s : Some errors occurred.\n", now());
+        return;
+    }
+    
+    if (density > 0) {
+        RS_set_density(H, density);
+    }
+    
+    RS_set_dsd_to_mp(H);
+    
+    RS_add_debris(H, OBJConfigLeaf, 100 * 1024);
+    
+    RS_revise_debris_counts_to_gpu_preference(H);
+    
+    RS_show_radar_params(H);
+    
+    RS_populate(H);
+    
+    printf("\nTest(s) using %s meteorological scatterers and %s debris objects for %s iterations:\n\n",
+           commaint(H->counts[0]), commaint(H->counts[1]), commaint(N));
+    
+    RS_advance_time(H);
+    
+    if (flag & TEST_SIMPLE) {
+        printf("Framework functions:\n\n");
+    }
+    
+    //
+    // RS_io_test()
+    //
+    if (flag & TEST_IO) {
+        byte_size = H->num_scats * 2 * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++)
+        RS_io_test(H);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "RS_io_test()", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  RS_advance_time()
+    //
+    if (flag & TEST_ADVANCE_TIME_GPU) {
+        // 4 Input arguments for scat_mov() kernel
+        byte_size = H->num_scats * 4 * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++)
+        RS_advance_time(H);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "RS_advance_time()", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  RS_make_pulse()
+    //
+    if (flag & TEST_MAKE_PULSE_GPU) {
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++)
+        RS_make_pulse(H);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT, "RS_make_pulse()", 1.0e3f * dt);
+    }
+    
+    //
+    //  RS_download()
+    //
+    if (flag & TEST_DOWNLOAD) {
+        // Only range_count * H + V - IQ data
+        byte_size = 5 * H->num_scats * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++)
+        RS_download(H);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "RS_download()", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  Some kernels
+    //
+    if (flag & TEST_KERNEL_MASK) {
+        printf("\nInternal kernel functions:\n\n");
+    }
+    
+    //
+    //  make_pulse_pass_1
+    //
+    if (flag & TEST_MAKE_PULSE_GPU_PASS_1) {
+        byte_size = H->workers[0].make_pulse_params.entry_counts[0] * 2 * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_make_pulse_pass_1,
+                                   1,
+                                   NULL,
+                                   &H->workers[0].make_pulse_params.global[0],
+                                   &H->workers[0].make_pulse_params.local[0],
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "make_pulse_pass_2", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  make_pulse_pass_2
+    //
+    if (flag & TEST_MAKE_PULSE_GPU_PASS_2) {
+        byte_size = H->workers[0].make_pulse_params.entry_counts[1] * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_make_pulse_pass_2,
+                                   1,
+                                   NULL,
+                                   &H->workers[0].make_pulse_params.global[1],
+                                   &H->workers[0].make_pulse_params.local[1],
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "make_pulse_pass_2", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  bg_atts
+    //
+    if (flag & TEST_BG_ATTS) {
+        byte_size = 4 * H->counts[0] * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_bg_atts,
+                                   1,
+                                   &H->workers[0].origins[0],
+                                   &H->workers[0].counts[0],
+                                   NULL,
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "bg_atts", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+
+    //
+    //  el_atts
+    //
+    if (flag & TEST_EL_ATTS) {
+        byte_size = 4 * H->counts[0] * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_el_atts,
+                                   1,
+                                   &H->workers[0].origins[0],
+                                   &H->workers[0].counts[0],
+                                   NULL,
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "el_atts", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  db_atts
+    //
+    if (flag & TEST_DB_ATTS) {
+        byte_size = 6 * H->counts[1] * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        int k = 1;
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_db_atts,
+                                   1,
+                                   &H->workers[0].origins[k],
+                                   &H->workers[0].counts[k],
+                                   NULL,
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "db_atts", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    //
+    //  scat_sig_aux
+    //
+    if (flag & TEST_SIG_AUX) {
+        byte_size = 4 * H->num_scats * sizeof(cl_float4);
+        gettimeofday(&t1, NULL);
+        for (i=0; i<N; i++) {
+            clEnqueueNDRangeKernel(H->workers[0].que,
+                                   H->workers[0].kern_scat_sig_aux,
+                                   1,
+                                   NULL,
+                                   &H->num_scats,
+                                   NULL,
+                                   0,
+                                   NULL,
+                                   NULL);
+        }
+        clFinish(H->workers[0].que);
+        gettimeofday(&t2, NULL);
+        dt = DTIME(t1, t2) / (float)N;
+        printf(FMT2, "scat_sig_aux", 1.0e3f * dt, 1.0e-9f * byte_size / dt);
+    }
+    
+    RS_free(H);}
 
 void RS_test_by_number(const int number, const void *arg) {
+    int verb = 0;
     switch (number) {
         case 0:
-            printf("Test 0.\n");
+            verb = *(int *)arg;
+            RS_test_by_flags(TEST_SIMPLE, verb);
+            break;
+        case 1:
+            verb = *(int *)arg;
+            RS_test_by_flags(TEST_KERNEL_MASK, verb);
+            break;
+        case 2:
+            verb = *(int *)arg;
+            RS_test_by_flags(TEST_ALL, verb);
+            break;
+        case 4:
+            printf("Debris flux field test.\n");
             set_debris_flux_field_to_test(NULL);
+            break;
+        case 11:
+            RS_test_by_flags(TEST_IO, verb);
+            break;
+        case 12:
+            RS_test_by_flags(TEST_ADVANCE_TIME_GPU, verb);
+            break;
+        case 13:
+            RS_test_by_flags(TEST_MAKE_PULSE_GPU, verb);
+            break;
+        case 14:
+            RS_test_by_flags(TEST_MAKE_PULSE_GPU_PASS_1, verb);
+            break;
+        case 15:
+            RS_test_by_flags(TEST_MAKE_PULSE_GPU_PASS_2, verb);
+            break;
+        case 16:
+            RS_test_by_flags(TEST_EL_ATTS, verb);
+            break;
+        case 17:
+            RS_test_by_flags(TEST_DB_ATTS, verb);
+            break;
+        case 18:
+            RS_test_by_flags(TEST_SIG_AUX, verb);
             break;
         default:
             printf("Test %d is invalid.\n", number);
             break;
     }
+}
+
+char *RS_test_by_number_description(const int indent) {
+    static char text[4096];
+    char helpText[] =
+    " 0 - Simple framework test using GPU(s)\n"
+    " 1 - Advanced framework tests using GPU(s)\n"
+    " 2 - Simple framework test using CPU\n"
+    " 3 - Advanced framework tests using both GPUs and CPUs\n"
+    "\n"
+    "Tests 11 - 18 are performance tests on specific CL kernels:\n"
+    "\n"
+    "11 - RS_io_test()\n"
+    "12 - RS_advanced_time()\n"
+    "13 - RS_make_pulse()\n"
+    "14 - RS_make_pulse_pass_1()\n"
+    "15 - RS_make_pulse_pass_2()\n"
+    "16 - RS_el_atts()\n"
+    "17 - RS_db_atts()\n"
+    "18 - scat_sig_aux()\n"
+    "\n"
+    " 1 - Generate iCDF from CDF\n";
+    RS_indent_copy(text, helpText, indent);
+    if (strlen(text) > 3000) {
+        fprintf(stderr, "Warning. Approaching limit. (%lu)\n", strlen(text));
+    }
+    return text;
 }
 
 static void show_help() {
@@ -191,9 +511,10 @@ static void show_help() {
            "  -t (--test) " UNDERLINE("value") "\n"
            "         Tests a specific component of the SimRadar framework.\n"
            "\n"
+           "%s"
+           "\n"
            "  -v (--verbose)\n"
            "         Increases verbosity level.\n"
-           "%s"
            "\n"
            "\n\n"
            "%s (SimRadar %s)\n\n",
@@ -245,7 +566,7 @@ int main(int argc, const char **argv) {
             case 't':
                 // A bunch of different tests
                 k = atoi(optarg);
-                RS_test_by_number(k, argc == optind ? NULL : argv[optind]);
+                RS_test_by_number(k, argc == optind ? (char *)&verbose : argv[optind]);
                 exit(EXIT_SUCCESS);
                 break;
             case 'v':
